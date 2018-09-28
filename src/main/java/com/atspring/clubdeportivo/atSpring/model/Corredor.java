75b71634741d08ce6ec195b2219c823731fe1966
Michael <galaxia1314@gmail.com>
2018-09-28 20:04:44 +0200

    clase Corredor en construccion

--- src/main/java/com/atspring/clubdeportivo/atSpring/model/Corredor.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/model/Corredor.java
@@ -1,5 +1,29 @@
 package com.atspring.clubdeportivo.atSpring.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Corredor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer idCorredor;
+	
+	private String nombre;
+	
+	private Integer dorsal;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Club club;
 
 }
