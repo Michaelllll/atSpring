8572c6acd4df5eae8588f97961c6b1aef16c3fe6
Michael <galaxia1314@gmail.com>
2018-09-28 19:16:35 +0200

    añadida configuracion

---
+++ src/main/java/com/atspring/clubdeportivo/atSpring/AtSpringApplication.java
@@ -0,0 +1,12 @@
+package com.atspring.clubdeportivo.atSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtSpringApplication.class, args);
+	}
+}
