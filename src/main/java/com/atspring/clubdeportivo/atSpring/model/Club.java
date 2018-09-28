187781e05b9af8ec248485ffb0f6b58531d40f78
Michael <galaxia1314@gmail.com>
2018-09-28 20:10:56 +0200

    clase Club construida

--- src/main/java/com/atspring/clubdeportivo/atSpring/model/Club.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/model/Club.java
@@ -1,5 +1,32 @@
 package com.atspring.clubdeportivo.atSpring.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Club {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer idClub;
+	
+	private Long puntuacion;
+	
+	private String nombre;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy="club")
+	private List<Corredor> corredores = new ArrayList<>();
 
 }
