93efefec0cbeca926d12eb0697186835d2fb6e61
Michael <galaxia1314@gmail.com>
2018-09-28 21:48:16 +0200

    clase CorredorPrueba en construccion

--- src/main/java/com/atspring/clubdeportivo/atSpring/model/CorredorPrueba.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/model/CorredorPrueba.java
@@ -1,6 +1,14 @@
 package com.atspring.clubdeportivo.atSpring.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,5 +18,34 @@ import lombok.Setter;
 @Setter
 public class CorredorPrueba {
 	
+	@Embeddable
+	public static class IdCorredorPrueba implements Serializable {
+
+		private static final long serialVersionUID = 817540279106537232L;
+
+		@Column(name = "fk_corredor")
+		private Integer idCorredor;
+
+		@Column(name = "fk_prueba")
+		private Integer idPrueba;
+
+		public IdCorredorPrueba() {}
+		
+		public IdCorredorPrueba(Integer idCorredor, Integer idPrueba) {
+			this.idCorredor = idCorredor;
+			this.idPrueba = idPrueba;
+		}
+	}
+	
+	@EmbeddedId
+	private IdCorredorPrueba idCorredorPrueba;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "fk_corredor", insertable = false, updatable = false)
+	private Corredor corredor;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "fk_prueba", insertable = false, updatable = false)
+	private Prueba prueba;
 	
 }
