66474a9a36d3982f1e9e0a781953eff40ecd4419
Michael <galaxia1314@gmail.com>
2018-09-28 20:52:47 +0200

    clase Prueba en construccion

--- src/main/java/com/atspring/clubdeportivo/atSpring/model/Prueba.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/model/Prueba.java
@@ -1,5 +1,24 @@
 package com.atspring.clubdeportivo.atSpring.model;
 
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 public class Prueba {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer idPrueba;
+	
+	private String nombre;
+	
+	private double distancia;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fecha;
 
 }
