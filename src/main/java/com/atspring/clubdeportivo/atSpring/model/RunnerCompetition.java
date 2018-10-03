aac4f97f410cb8e768053866c896d0e29f641941
Michaelllll <galaxia1314@gmail.com>
2018-10-03 11:56:43 +0200

    Mapeo contra la BD reformado

--- src/main/java/com/atspring/clubdeportivo/atSpring/model/RunnerCompetition.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/model/RunnerCompetition.java
@@ -1,55 +1,40 @@
 package com.atspring.clubdeportivo.atSpring.model;
 
-import java.io.Serializable;
-
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@Table(uniqueConstraints = @UniqueConstraint(columnNames={"fk_runner","fk_competition"}))
 @Entity
 @Getter
 @Setter
 public class RunnerCompetition {
 	
-	@Embeddable
-	@Getter
-	@Setter
-	public static class IdRunnerCompetition implements Serializable {
-
-		private static final long serialVersionUID = 817540279106537232L;
-
-		@Column(name = "fk_runner")
-		private Integer idRunner;
-
-		@Column(name = "fk_competition")
-		private Integer idCompetition;
-
-		public IdRunnerCompetition() {}
-		
-		public IdRunnerCompetition(Integer idRunner, Integer idCompetition) {
-			this.idRunner = idRunner;
-			this.idCompetition = idCompetition;
-		}
-	}
+	@Id
+	@NotNull
+	@GeneratedValue(strategy = GenerationType.AUTO )
+	private Integer idRunnerCompetition;
 	
-	@EmbeddedId
-	private IdRunnerCompetition idRunnerCompetition;
+	private Integer seconds;
 
+	@NotNull
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "fk_runner")
 	private Runner runner;
 	
+	@NotNull
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "fk_competition")
 	private Competition competition;
-	
-	private Integer seconds;
 	
 }
