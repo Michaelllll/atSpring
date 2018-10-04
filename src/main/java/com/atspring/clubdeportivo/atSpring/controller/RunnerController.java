396934bc88ce05a4c66bea2de2b860eddd7ada0f
Michaelllll <galaxia1314@gmail.com>
2018-10-04 11:55:48 +0200

    Nuevas operaciones de REST implementadas

--- src/main/java/com/atspring/clubdeportivo/atSpring/controller/RunnerController.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/controller/RunnerController.java
@@ -3,10 +3,13 @@ package com.atspring.clubdeportivo.atSpring.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.atspring.clubdeportivo.atSpring.dto.RunnerDTO;
@@ -25,9 +28,38 @@ public class RunnerController {
 	RunnerMapper mapper;
 
 	@GetMapping("/{id}")
-	RunnerDTO findRunnerById(@PathVariable Integer id)
+	public RunnerDTO findRunnerById (@PathVariable Integer id)
 	{
 		Optional<Runner> runner = runnerService.findById(id);
+		return mapper.mapToDTO(runner);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable Integer id)
+	{
+		runnerService.delete(id);
+	}
+	
+	@PutMapping("/{id}")
+	public void update (@RequestBody RunnerDTO dto, @PathVariable Integer id) throws Exception
+	{
+		if(id==dto.getIdRunner() && id!=null)
+		{
+			Optional<Runner> runner = runnerService.findById(id);
+			runner.get().setName(dto.getName());
+			runner.get().setBirthDate(dto.getBirthDate());
+			runner.get().setNumber(dto.getNumber());
+			runner.get().setClub(dto.getClub());	
+			runnerService.update(runner.get());
+		}
+		else
+			throw new Exception("El id no existe o no coincide con el corredor.");
+	}
+	
+	@PostMapping("/{id}")
+	public RunnerDTO create (@RequestBody RunnerDTO dto)
+	{
+		Runner runner = runnerService.create(mapper.mapToModel(dto));
 		return mapper.mapToDTO(runner);
 	}
 }
