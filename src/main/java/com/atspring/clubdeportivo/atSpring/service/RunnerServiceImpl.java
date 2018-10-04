c6ade33788453a31dff872db5868df013a213c45
Michaelllll <galaxia1314@gmail.com>
2018-10-04 11:54:52 +0200

    Código ordenado para mejor lectura

--- src/main/java/com/atspring/clubdeportivo/atSpring/service/RunnerServiceImpl.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/service/RunnerServiceImpl.java
@@ -15,25 +15,14 @@ public class RunnerServiceImpl implements RunnerService{
 	@Autowired
 	RunnerDAO dao;
 
-	public Runner create(Runner runner) {
-		return dao.save(runner);
-	}
+	public Runner create(Runner runner) { return dao.save(runner); }
 
-	public void update(Runner runner) {
-		dao.saveAndFlush(runner);
-	}
+	public void update(Runner runner) { dao.saveAndFlush(runner); }
 
-	public void delete(Integer idRunner) {
-		
-		dao.deleteById(idRunner);
-	}
+	public void delete(Integer idRunner) { dao.deleteById(idRunner); }
 
-	public Optional<Runner> findById(Integer idRunner) {
-		return dao.findById(idRunner);
-	}
+	public Optional<Runner> findById(Integer idRunner) { return dao.findById(idRunner); }
 
-	public List<Runner> findAll() {
-		return dao.findAll();
-	}
+	public List<Runner> findAll() { return dao.findAll(); }
 
 }
