2be4042315ae4359ab4565c9e646fad6cb226769
Michaelllll <galaxia1314@gmail.com>
2018-10-04 11:56:51 +0200

    Añadido nuevo atributo al DTO

--- src/main/java/com/atspring/clubdeportivo/atSpring/dto/RunnerDTO.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/dto/RunnerDTO.java
@@ -1,13 +1,47 @@
 package com.atspring.clubdeportivo.atSpring.dto;
 
+import java.util.Date;
+
+import com.atspring.clubdeportivo.atSpring.model.Club;
+
 import lombok.Data;
 
 @Data
 public class RunnerDTO {
 	
+	private Integer idRunner;
+	
 	private String name;
 	
+	private Date birthDate;
+	
 	private Integer number;
+	
+	private Club club;
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Club getClub() {
+		return club;
+	}
+
+	public void setClub(Club club) {
+		this.club = club;
+	}
+
+	public Integer getIdRunner() {
+		return idRunner;
+	}
+
+	public void setIdRunner(Integer idRunner) {
+		this.idRunner = idRunner;
+	}
 
 	public String getName() {
 		return name;
