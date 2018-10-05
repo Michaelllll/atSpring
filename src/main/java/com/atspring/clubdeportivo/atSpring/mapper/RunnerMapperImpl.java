1140cd454c9dbacfc478f2f856fa91193780acc6
Michaelllll <galaxia1314@gmail.com>
2018-10-05 11:41:07 +0200

    Eliminación de uso de un atributo que no está en los requisitos de información

--- src/main/java/com/atspring/clubdeportivo/atSpring/mapper/RunnerMapperImpl.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/mapper/RunnerMapperImpl.java
@@ -15,7 +15,6 @@ public class RunnerMapperImpl implements RunnerMapper{
 	public Runner mapToModel(RunnerDTO dto) {
 		final Runner runner = new Runner();
 		runner.setName(dto.getName());
-		runner.setNumber(dto.getNumber());
 		runner.setBirthDate(dto.getBirthDate());
 		runner.setClub(dto.getClub());
 		return runner;
@@ -24,7 +23,6 @@ public class RunnerMapperImpl implements RunnerMapper{
 	public RunnerDTO mapToDTO(Optional<Runner> runner) {
 		final RunnerDTO dto = new RunnerDTO();
 		dto.setName(runner.get().getName());
-		dto.setNumber(runner.get().getNumber());
 		dto.setBirthDate(runner.get().getBirthDate());
 		dto.setClub(runner.get().getClub());
 		return dto;
@@ -33,7 +31,6 @@ public class RunnerMapperImpl implements RunnerMapper{
 	public RunnerDTO mapToDTO(Runner runner) {
 		final RunnerDTO dto = new RunnerDTO();
 		dto.setName(runner.getName());
-		dto.setNumber(runner.getNumber());
 		dto.setBirthDate(runner.getBirthDate());
 		dto.setClub(runner.getClub());
 		return dto;
