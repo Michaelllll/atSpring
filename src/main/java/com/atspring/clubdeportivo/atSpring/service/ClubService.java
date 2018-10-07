9ba00dff8fefbd13bbeb64941d84226034dfecd8
Michaelllll <galaxia1314@gmail.com>
2018-10-08 00:18:30 +0200

    Añdidos comentarios

--- src/main/java/com/atspring/clubdeportivo/atSpring/service/ClubService.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/service/ClubService.java
@@ -4,8 +4,25 @@ import com.atspring.clubdeportivo.atSpring.model.Club;
 
 public interface ClubService {
 
+	/**
+	 * Da de alta un nuevo club.
+	 * @param club
+	 * @return el nuevo club.
+	 */
 	Club create(Club club);
+	
+	/**
+	 * Asocia un corredor con el club.
+	 * @param idClub indentificador del club.
+	 * @param idRunner identificador del corredor.
+	 */
 	void addRunner(Integer idClub, Integer idRunner);
+	
+	/**
+	 * Busca un club.
+	 * @param idClub identificador del club.
+	 * @return el club o en su defecto un optional vacío.
+	 */
 	Club findById(Integer idClub);
 
 }
