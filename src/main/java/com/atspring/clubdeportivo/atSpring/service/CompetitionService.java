c0ddb254b5adbcac4a3c76d32c4d80a8f6ec0c9b
Michaelllll <galaxia1314@gmail.com>
2018-10-08 00:21:46 +0200

    Añadido comentarios

--- src/main/java/com/atspring/clubdeportivo/atSpring/service/CompetitionService.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/service/CompetitionService.java
@@ -6,6 +6,17 @@ import com.atspring.clubdeportivo.atSpring.model.Competition;
 
 public interface CompetitionService {
 
+	/**
+	 * Crea una nueva competición.
+	 * @param competition la competición a dar de alta.
+	 * @return la competición creada.
+	 */
 	Competition create(Competition competition);
+	
+	/**
+	 * Buscar una competición.
+	 * @param idCompetition identificador de la competición.
+	 * @return la competición encontrada o en su defecto un optional vacio.
+	 */
 	Optional<Competition> findById (Integer idCompetition);
 }
