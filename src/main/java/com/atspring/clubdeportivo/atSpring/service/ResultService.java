cbeccd5930f65ecd063e53d2471269c228cb13b8
Michaelllll <galaxia1314@gmail.com>
2018-10-07 16:43:16 +0200

    Escritos comentarios sobre los métodos

--- src/main/java/com/atspring/clubdeportivo/atSpring/service/ResultService.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/service/ResultService.java
@@ -10,15 +10,47 @@ import com.atspring.clubdeportivo.atSpring.model.Result;
 
 public interface ResultService {
 
+	/**
+	 * Da de alta un nuevo resultado, dado sus respectivos atributos.
+	 * @param result el Resultado a guardar.
+	 * @param idRunner el identificador del corredor con el que será relacionado.
+	 * @param idCompetition el identificador de la competición a la que pertenece el resultado.
+	 * @return
+	 */
 	Result create(Result result, Integer idRunner, Integer idCompetition);
 
+	/**
+	 * Dado un identificador proveniente de un resultado, obtiene el objeto Result al que corresponde.
+	 * @param idResult identificador de resultado.
+	 * @return el resultado encontrado, de tipo Optional<Result>
+	 */
 	Optional<Result> findById(Integer idResult);
 
+	/**
+	 * Obtiene todos los resultados disponibles.
+	 * @return una lista de resultados de tipo Result.
+	 */
 	List<Result> findAll();
 	
+	/**
+	 * Obtiene las puntuaciones que se establecen para la competición dada a partir de ScoreDAO.
+	 * @param idCompetition identificador de la competición.
+	 * @return una lista de puntuaciones de tipo ScoreDTO.
+	 */
 	List<ScoreDTO> getScores(Integer idCompetition);
 	
+	/**
+	 * Obtiene los resultados de una competición a partir de ResultDAO.
+	 * @param idCompetition identificador de la competición.
+	 * @return una lista de resultados de tipo ResultDTO.
+	 */
 	List<ResultDTO> getResults(Integer idCompetition);
 	
+	/**
+	 * Recupera las puntuaciones y los participantes de una prueba o competición y crea una clasficación
+	 * en base a los puntos, posteriormente se almacenan en un fichero.	
+	 * @param idCompetition identificador de la competición.
+	 * @throws IOException
+	 */
 	void writeToFile(Integer idCompetition) throws IOException;
 }
