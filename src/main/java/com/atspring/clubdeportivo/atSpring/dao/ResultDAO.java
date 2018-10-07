2dc57172090cee580426d2386dfd8b1c44ebe3f7
Michaelllll <galaxia1314@gmail.com>
2018-10-07 11:24:38 +0200

    Nueva consulta en JPQL para obtener los clubes a los que pertenecen los corredores que han participado en una competición dada

--- src/main/java/com/atspring/clubdeportivo/atSpring/dao/ResultDAO.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/dao/ResultDAO.java
@@ -1,11 +1,22 @@
 package com.atspring.clubdeportivo.atSpring.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.atspring.clubdeportivo.atSpring.dto.ResultDTO;
 import com.atspring.clubdeportivo.atSpring.model.Result;
 
 @Repository
 public interface ResultDAO extends JpaRepository<Result, Integer>{
 	
+	@Query("SELECT new com.atspring.clubdeportivo.atSpring.dto.ResultDTO(r.idResult, r.seconds, r.runner.idRunner, c) "
+			+ "from Result r "
+			+ "INNER JOIN r.runner.club c "
+			+ "where r.competition.idCompetition = :idCompetition "
+			+ "group by r.idResult "
+			+ "order by r.seconds ")
+	public List<ResultDTO> getResults(Integer idCompetition);
 }
--- src/main/java/com/atspring/clubdeportivo/atSpring/dto/ResultDTO.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/dto/ResultDTO.java
@@ -2,6 +2,8 @@ package com.atspring.clubdeportivo.atSpring.dto;
 
 import java.io.Serializable;
 
+import com.atspring.clubdeportivo.atSpring.model.Club;
+
 public class ResultDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -9,12 +11,32 @@ public class ResultDTO implements Serializable {
 	private Integer idResult;
 
 	private Integer seconds;
-	
-	private Integer idRunner;
-	
-	private Integer idCompetition;
 
+	private Integer idRunner;
+
+	private Integer idCompetition;
 	
+	private Club club;
+
+	public ResultDTO(Integer idResult, Integer seconds, Integer idRunner, Club club) {
+		super();
+		this.idResult = idResult;
+		this.seconds = seconds;
+		this.idRunner = idRunner;
+		this.club = club;
+	}
+
+	public ResultDTO() {
+	}
+
+	public Club getClub() {
+		return club;
+	}
+
+	public void setClub(Club club) {
+		this.club = club;
+	}
+
 	public Integer getIdCompetition() {
 		return idCompetition;
 	}
@@ -46,5 +68,5 @@ public class ResultDTO implements Serializable {
 	public void setIdRunner(Integer idRunner) {
 		this.idRunner = idRunner;
 	}
-	
+
 }
