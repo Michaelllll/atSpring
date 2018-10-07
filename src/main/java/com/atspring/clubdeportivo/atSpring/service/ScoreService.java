93690ba36fb79bfb52b5c10dbdb07ebc16c4b8da
Michaelllll <galaxia1314@gmail.com>
2018-10-08 00:29:29 +0200

    Añadidos comentarios

--- src/main/java/com/atspring/clubdeportivo/atSpring/service/ScoreService.java
+++ src/main/java/com/atspring/clubdeportivo/atSpring/service/ScoreService.java
@@ -6,5 +6,10 @@ import com.atspring.clubdeportivo.atSpring.dto.ScoreDTO;
 
 public interface ScoreService {
 	
+	/**
+	 * Obtiene la puntuación que corresponde a una competición dada
+	 * @param idCompetition identificador de la competición
+	 * @return lista de puntuaciones
+	 */
 	List<ScoreDTO> findAll(Integer idCompetition);
 }
